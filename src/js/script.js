24590c3b9e714682e3da2b20cf3f735d83290d27
Sławek Kopytek <slavkopytek@gmail.com>
2022-01-27 17:27:20 +0100

    10.2

---
+++ src/js/script.js
@@ -0,0 +1,62 @@
+{
+  'use strict';
+  
+  const select = {
+    templateOf: {
+      bookTemplate: '#template-book',
+    },
+    containerOf: {
+      bookList: '.books-list',
+      images: '.book__image',
+    },
+  };
+  
+  const templates = {
+    bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML)
+  };
+  
+  function render(){ 
+    /* for each elements */
+    for(let eachBook of dataSource.books){
+  
+      /* generate HTML based on template*/
+      const generatedHTML = templates.bookTemplate(eachBook);
+  
+      /* create element using utils.createElementFromHTML */
+      const element = utils.createDOMFromHTML(generatedHTML);
+  
+      /* find book container */
+      const bookListContainer = document.querySelector(select.containerOf.bookList);
+  
+      /* add book to menu */
+      bookListContainer.appendChild(element);
+  
+    } 
+  }
+  const booksList = document.querySelector('.books-list');
+  
+  const favoriteBooks = [];
+  function initAction(){
+    
+    const booksImages = document.querySelectorAll(select.containerOf.images);
+    console.log(booksImages);
+
+    for(let image of booksImages){
+      image.addEventListener('dbclick', function(event){
+        event.preventDefault();
+
+        image.classList.add('favorite');
+        const bookId = booksList.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+        
+      });
+    }
+
+    
+
+  }
+
+  render();
+  initAction();
+  
+} 
